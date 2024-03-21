10ea40bd48f10b95648c451fadb7daae1010c9ef
José Rodrigo da Silva Inácio <jose.inacio@cwi.com.br>
2024-03-21 00:08:19 -0300

    Code

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use std::convert::Infallible;
+
+async fn handle_request(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Hello, World!")))
+}
+
+#[tokio::main]
+async fn main() {
+    let make_svc = make_service_fn(|_conn| {
+        async { Ok::<_, Infallible>(service_fn(handle_request)) }
+    });
+
+    let addr = ([127, 0, 0, 1], 3000).into();
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
